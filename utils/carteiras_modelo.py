1e7a9641e532c32a8be0bb1c39b2cbefd07b6516
Fonck2611 <brunodppfonseca@hotmail.com>
2025-07-01 17:42:41 -0300

    Update carteiras_modelo.py

--- utils/carteiras_modelo.py
+++ utils/carteiras_modelo.py
@@ -1,16 +1,16 @@
 def get_modelo_carteira(tipo):
     modelos = {
         "Conservadora": {
-            "Pós-Fixado": 70,
-            "Pré-Fixado": 5,
+            "Pós Fixado": 70,
+            "Pré Fixado": 5,
             "Inflação": 15,
             "Renda Fixa Global": 10
         },
         "Moderada": {
-            "Pós-Fixado": 35,
-            "Pré-Fixado": 7.5,
+            "Pós Fixado": 35,
+            "Pré Fixado": 7.5,
             "Inflação": 20,
-            "Multimercados": 5,
+            "Multimercado": 5,
             "Renda Variável Brasil": 10,
             "Fundos Listados": 5,
             "Alternativos": 2.5,
@@ -18,10 +18,10 @@ def get_modelo_carteira(tipo):
             "Renda Variável Global": 5
         },
         "Sofisticada": {
-            "Pós-Fixado": 15,
-            "Pré-Fixado": 10,
+            "Pós Fixado": 15,
+            "Pré Fixado": 10,
             "Inflação": 25,
-            "Multimercados": 5,
+            "Multimercado": 5,
             "Renda Variável Brasil": 15,
             "Fundos Listados": 7.5,
             "Alternativos": 7.5,
